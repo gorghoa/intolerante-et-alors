f353ab53d00b6b34e591dd033c9176657c26ba74
Grégory Copin <gregcop1@gmail.com>
2017-01-29 16:26:10 +0100

    feat: style first steps

---
+++ wp-content/themes/skin/template-parts/header/header-image.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Displays header media
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="custom-header">
+
+
+</div><!-- .custom-header -->
