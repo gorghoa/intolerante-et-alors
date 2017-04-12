e84631a0808a7c21c65981041b13add529003911
Rodrigue Villetard <rodrigue@les-tilleuls.coop>
2017-04-12 14:23:00 +0200

    Update main.js
    fix the scrolling on firefox, untested on chrome :-p

--- wp-content/themes/skin/assets/js/main.js
+++ wp-content/themes/skin/assets/js/main.js
@@ -2,7 +2,7 @@
 
     function arePostsVisible() {
         var deviceHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-        var limit = document.body.scrollTop + (deviceHeight / 2);
+        var limit = window.scrollY + (deviceHeight / 2);
         document.querySelectorAll('.post-list .post:not(.post--visible)').forEach(function(node) {
             if (node.offsetTop < limit) {
                 node.classList.add('post--visible')
